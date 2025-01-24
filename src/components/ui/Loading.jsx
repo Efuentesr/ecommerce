3f07ef4b412e7086f7e271126ffac2797779cbd6
Edmundo FuentesRivera <edmundo.fuentesrivera@frappurecords.com>
2025-01-23 20:54:57 -0500

    efrm

---
+++ src/components/ui/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+        <i className="fa-solid fa-spinner fa-6x fa-spin" ></i>
+    </div>
+  )
+}
+
+export default Loading
